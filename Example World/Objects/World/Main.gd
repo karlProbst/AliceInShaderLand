1806f5ade33557f139d86e9744d0bc3a18e29288
@karlProbst <karlprobst@protonmail.com>
2024-05-13 22:28:12 -0300

    dialog!

---
+++ Example World/Objects/World/Main.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func Dialog_Gato_1():
+	#CHAMAR DIALOGO 1 do gato
+	pass
